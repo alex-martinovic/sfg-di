e5d03307d27860483b7c2e3508a8e588e0e552f9
Alex Martinovic <40226785+alex-martinovic@users.noreply.github.com>
2021-07-12 11:55:51 -0700

    Property Binding with Configuration class

--- src/main/java/com/martinovic/sfgdi/SfgDiApplication.java
+++ src/main/java/com/martinovic/sfgdi/SfgDiApplication.java
@@ -1,5 +1,6 @@
 package com.martinovic.sfgdi;
 
+import com.martinovic.sfgdi.config.SfgConfiguration;
 import com.martinovic.sfgdi.controllers.*;
 import com.martinovic.sfgdi.datasource.FakeDataSource;
 import com.martinovic.sfgdi.services.PrototypeBean;
@@ -50,9 +51,16 @@ public class SfgDiApplication {
 		PrototypeBean prototypeBean2 = ctx.getBean(PrototypeBean.class);
 		System.out.println(prototypeBean2.getMyScope());
 
+		System.out.println("----- Fake Data Source -----");
 		FakeDataSource fakeDataSource = ctx.getBean(FakeDataSource.class);
 		System.out.println(fakeDataSource.getUsername());
 		System.out.println(fakeDataSource.getPassword());
 		System.out.println(fakeDataSource.getJdbcUrl());
+
+		System.out.println("----- Config Props Bean -----");
+		SfgConfiguration sfgConfiguration = ctx.getBean(SfgConfiguration.class);
+		System.out.println(sfgConfiguration.getUsername());
+		System.out.println(sfgConfiguration.getPassword());
+		System.out.println(sfgConfiguration.getJdbcUrl());
 	}
 }
